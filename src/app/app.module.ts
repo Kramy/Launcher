bc6dff643ddf0df823739713c2f68251101597b5
Rafael Benavides Terrón <rabenter@gmail.com>
2017-11-30 18:06:01 +0100

    - Added frame component. - Minos changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FrameComponent } from './frame/frame.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrameComponent
   ],
   imports: [
     BrowserModule
