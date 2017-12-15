4a4133b0ad8c00b8c746662a6a94aa93f1d5ba4d
Rafael Benavides <rabenter@gmail.com>
2017-12-16 00:20:25 +0100

    - Minor changes.

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games.component.scss']
 })
 export class GamesComponent implements OnInit {
+  public icon: string = "assets/img/game.png";
 
   constructor() { }
 
